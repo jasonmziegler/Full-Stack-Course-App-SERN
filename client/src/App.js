c5f1e5aebcba39b803629f02c311e74a0ab4bd10
Jason Ziegler <jason@insightfulautomation.com>
2022-05-28 22:58:52 -0500

    installed react-router-dom, created Courses Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 //import logo from './logo.svg';
 import './styles/App.css';
 
 import Header from './components/Header';
+import Courses from './components/Courses';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <Routes>
+        <Route exact path='/' component={Courses} />
+      </Routes>
+    </Router>
+      
+    
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends React.PureComponent {
+
     render() {
         return(
-            <header className="App-header">
-        <p>
-          Team Treehouse Final Project - Course App
-        </p>
-        
-</header>
+            <div className="header">
+                <div className="bounds">
+                <h1 className="header--logo">TeamTreeHouse Final Project - Course App</h1>
+                
+                <nav>
+                                        
+                    <React.Fragment>
+                        <span>Welcome, UserName!</span>
+                        <Link to="/">Sign Out</Link>
+                        <Link className="signup" to="/">Sign Up</Link>
+                        <Link className="signin" to="/">Sign In</Link>
+                    </React.Fragment>
+                    
+                </nav>
+                </div>
+            </div>
         )
     }
 }
