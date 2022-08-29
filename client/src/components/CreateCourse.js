b8238fd17eba9ef453540dba757bd60b95e141db
Jason Ziegler <jason@insightfulautomation.com>
2022-08-29 09:11:55 -0500

    added react form elements to create course

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
+import Form from './Form';
+
 class CreateCourse extends Component {
+    state = {
+        courseTitle: '',
+        courseDescription: '',
+        materialsNeeded: '',
+        estimatedTime: '',
+        userId: 1,
+        errors: [],
+      }
     render() {
+        const {
+            courseTitle,
+            courseDescription,
+            materialsNeeded,
+            estimatedTime,
+            userId,
+            errors,
+          } = this.state;
         return (
             <main>
                 <div className="wrap">
@@ -13,31 +31,77 @@ class CreateCourse extends Component {
                             <li>Please provide a value for "Description"</li>
                         </ul>
                     </div>
-                    <form>
-                        <div className="main--flex">
-                            <div>
-                                <label htmlFor="courseTitle">Course Title</label>
-                                {/* <input id="courseTitle" name="courseTitle" type="text" value=""> */}
-
-                                <p>By Joe Smith</p>
-
-                                <label htmlFor="courseDescription">Course Description</label>
-                                <textarea id="courseDescription" name="courseDescription"></textarea>
-                            </div>
-                            <div>
-                                <label htmlFor="estimatedTime">Estimated Time</label>
-                                {/* <input id="estimatedTime" name="estimatedTime" type="text" value=""> */}
-
-                                <label htmlFor="materialsNeeded">Materials Needed</label>
-                                <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
-                            </div>
-                        </div>
-                        <button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
-                    </form>
+                    <div className="main--flex">
+                        <Form 
+                            cancel={this.cancel}
+                            errors={errors}
+                            submit={this.submit}
+                            submitButtonText="Create Course"
+                            elements={() => (
+                            <React.Fragment>
+                                <div>
+                                    <label htmlFor="courseTitle">Course Title</label>
+                                    <input 
+                                        id="courseTitle" 
+                                        name="courseTitle" 
+                                        type="text"
+                                        value={courseTitle} 
+                                        onChange={this.change} 
+                                        placeholder="Course Title" />
+                                    <label htmlFor="courseDescription">Course Description</label>
+                                    <textarea 
+                                        id="courseDescription" 
+                                        name="courseDescription" 
+                                        value={courseDescription} 
+                                        onChange={this.change} 
+                                        placeholder="Course Description" />
+                                </div>
+                                <div>
+                                    <label htmlFor="estimatedTime">Estimated Time</label>
+                                    <input 
+                                        id="estimatedTime" 
+                                        name="estimatedTime" 
+                                        type="text"
+                                        value={estimatedTime} 
+                                        onChange={this.change} 
+                                        placeholder="Estimated Time" />
+                                    <label htmlFor="materialsNeeded">Materials Needed</label>
+                                    <textarea 
+                                        id="materialsNeeded" 
+                                        name="materialsNeeded"
+                                        type="materialsNeeded"
+                                        value={materialsNeeded} 
+                                        onChange={this.change} 
+                                        placeholder="Materials Needed" />
+                                </div>
+                            </React.Fragment>
+                        )} />
+                            
+                    </div>
                 </div>
             </main>
             )
         }
+
+        change = (event) => {
+            const name = event.target.name;
+            const value = event.target.value;
+        
+            this.setState(() => {
+              return {
+                [name]: value
+              };
+            });
+        }
+
+        submit = () => {
+            console.log("Form Submitted");
+        }
+        
+        cancel = () => {
+        console.log("Form Cancelled");
+        }
+
     }
 
 export default CreateCourse;       
