93236feafce6ebc194e7438beaeba77165925242
Jason Ziegler <jason@insightfulautomation.com>
2022-09-06 15:15:07 -0500

    converted SignIn Component to a functional stateful functional component

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,79 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Form from './Form';
 import { Link } from 'react-router-dom';
 
-class SignIn extends Component {
-    state = {
-        username: '',
-        password: '',
-        errors: [],
-      }
-    render() { 
-        const {
-            username,
-            password,
-            errors,
-          } = this.state;
-        return (
-            <main>
-                <div className="form--centered">
-                    <h2>Sign In</h2>
-                    <Form 
-                        cancel={this.cancel}
-                        errors={errors}
-                        submit={this.submit}
-                        submitButtonText="Sign In"
-                        elements={() => (
-                            <React.Fragment>
-                                <label htmlFor="emailAddress">Email Address</label>
-                                <input 
-                                id="username" 
-                                name="username" 
-                                type="text"
-                                value={username} 
-                                onChange={this.change} 
-                                placeholder="User Name" />
-                                <label htmlFor="password">Password</label>
-                                <input 
-                                id="password" 
-                                name="password"
-                                type="password"
-                                value={password} 
-                                onChange={this.change} 
-                                placeholder="Password" />                
-                            </React.Fragment>
-                        )} />
-                    {/* <form> */}
-                        
-                        {/* <input id="emailAddress" name="emailAddress" type="email" value=""> */}
-                        
-                        {/* <input id="password" name="password" type="password" value=""> */}
-                        {/* // <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick="event.preventDefault(); location.href='index.html';">Cancel</button> */}
-                    {/* </form> */}
-                    <p>Don't have a user account? Click here to <Link to="/sign-up">sign up</Link>!</p>
-                    
-                </div>
-            </main>
-        )
-    }
-    change = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-    
-        this.setState(() => {
-          return {
-            [name]: value
-          };
-        });
-      }
-    
-      submit = () => {
-        console.log("Form Submitted");
-      }
-    
-      cancel = () => {
-        console.log("Form Cancelled");
-      }
+// NEED TO TURN INTO AN ARROW FUNCTION not a class component but a "Arrow Function Component"
+// Need to use "useState" 
+// Need to use useNavigate
+const  SignIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  // state = {
+  //     username: '',
+  //     password: '',
+  //     errors: [],
+  //   }
+  // render() { 
+  //     const {
+  //         username,
+  //         password,
+  //         errors,
+  //       } = this.state;
+  // const change = (event) => {
+  //   const name = event.target.name;
+  //   const value = event.target.value;
+
+  //   this.setState(() => {
+  //     return {
+  //       [name]: value
+  //     };
+  //   });
+  // }
+
+  const submit = () => {
+    console.log("Form Submitted");
+  }
+
+  const cancel = () => {
+    console.log("Form Cancelled");
+  }
+  
+  return (
+    <main>
+      <div className="form--centered">
+          <h2>Sign In</h2>
+          <Form 
+            cancel={cancel}
+            errors={errors}
+            submit={submit}
+            submitButtonText="Sign In"
+            elements={() => (
+              <React.Fragment>
+                  <label htmlFor="emailAddress">Email Address</label>
+                  <input 
+                  id="username" 
+                  name="username" 
+                  type="text"
+                  value={username} 
+                  onChange={(e) => {setUsername(e.target.value)}} 
+                  placeholder="User Name" />
+                  <label htmlFor="password">Password</label>
+                  <input 
+                  id="password" 
+                  name="password"
+                  type="password"
+                  value={password} 
+                  onChange={(e) => {setPassword(e.target.value)}} 
+                  placeholder="Password" />                
+              </React.Fragment>
+            )} 
+          />
+          <p>Don't have a user account? Click here to <Link to="/sign-up">sign up</Link>!</p>
+      </div>
+    </main>
+  )
 }
 
 export default SignIn;
