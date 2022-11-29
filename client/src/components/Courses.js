ebe926d787e54c946f9f999f3cf1dd2b86737457
Jason Ziegler <jason@insightfulautomation.com>
2022-11-29 07:24:26 -0600

    moved old code to bottom of Courses.js file

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -6,10 +6,59 @@ import Course from './Course';
 
 import { v4 as uuidv4 } from 'uuid';
 
-//const axios = require('axios');
-
 const Courses = (props) => {
-  // constructor() {
+  
+  const  { context } = props;
+  const [loading, setLoading] = useState(true);
+  const [courses, updateCourses] = useState([]);
+
+//adapted from https://dev.to/darkmavis1980/fetching-data-with-react-hooks-and-axios-114h
+
+const fetchCoursesHandler = () => {
+  setLoading(true);
+  context.data.getCourses()
+  .then(data => {
+    console.log(data);
+    updateCourses(data);
+    setLoading(false);
+  })
+  .catch((error => {
+    console.error(error.message);
+  }));
+}
+
+  useEffect(() => {
+
+    fetchCoursesHandler();
+    
+  }, []);
+
+    return (
+    
+      <main>
+        {loading && <div>Loading</div>}
+        {!loading && (
+          <div className="wrap main--grid">
+            {courses.map(course => (<Course id={course.id} title={course.title} key={uuidv4()}/>))}
+            <Link className="course--module course--add--module" to="/courses/create" >
+              <span className="course--add--title">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                  viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                  New Course
+              </span>
+          </Link>
+          </div>)
+        }  
+      </main>
+    );
+  }
+
+
+export default Courses;
+
+
+// OLD CODE
+// constructor() {
   //   super();
   //   this.state = {
   //     courses: [],
@@ -37,53 +86,4 @@ const Courses = (props) => {
   // render() {
   //   console.log('Courses: ', this.state.courses);
   //   let courses = this.state.courses;
-  const  { context } = props;
-  const [loading, setLoading] = useState(true);
-  const [courses, updateCourses] = useState([]);
-
-//adapted from https://dev.to/darkmavis1980/fetching-data-with-react-hooks-and-axios-114h
-
-const fetchCoursesHandler = () => {
-  setLoading(true);
-  context.data.getCourses()
-  .then(data => {
-    console.log(data);
-    updateCourses(data);
-    setLoading(false);
-  })
-  .catch((error => {
-    console.error(error.message);
-  }));
-}
-
-  useEffect(() => {
-
-    fetchCoursesHandler();
-    
-  }, []);
-
-    return (
-      
-      <main>
-        
-        {loading && <div>Loading</div>}
-        {!loading && (
-          <div className="wrap main--grid">
-            {courses.map(course => (<Course id={course.id} title={course.title} key={uuidv4()}/>))}
-            <Link className="course--module course--add--module" to="/courses/create" >
-              <span className="course--add--title">
-                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                  viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-                  New Course
-              </span>
-          </Link>
-          </div>)
-        }
-          
-      
-      </main>
-    );
-  }
-
-
-export default Courses;
+  //const axios = require('axios');
